3f1f507df3d7039954c5329eef2621a3a1c8d60f
islem mellehi <islemmellehi222@gmail.com>
2023-02-08 21:12:38 +0100

    Home page half done

---
+++ Hotel Reservation App/Js/home.js
@@ -0,0 +1,92 @@
+const slides = document.querySelectorAll(".slide");
+
+var counter = 0;
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
+const goPrev = () => {
+  counter--;
+  slideImage();
+};
+const goNext = () => {
+  counter++;
+  slideImage();
+  if (counter == 4) {
+    counter = 0;
+  }
+  slideImage();
+};
+
+var header = document.getElementById("Background");
+
+function HeaderBgChange() {
+  header.style.backgroundColor = "#20262E";
+  if (scrollY == screenTop) {
+    header.style.backgroundColor = "transparent";
+  }
+}
+
+let clip = document.getElementById("videoAD");
+clip.addEventListener("mouseover", function (e) {
+  clip.play();
+});
+
+clip.addEventListener("mouseout", function (e) {
+  clip.pause();
+});
+
+let counts = setInterval(updated);
+let upto = 0;
+
+function updated() {
+  var count = document.getElementById("counter1");
+
+  count.innerHTML = ++upto;
+  if (upto === 1000) {
+    count.innerHTML = 9 + " M";
+    clearInterval(counts);
+  }
+}
+
+
+let counts2 = setInterval(updated2);
+let upto2 = 0;
+
+function updated2() {
+  var count2 = document.getElementById("counter2");
+
+  count2.innerHTML = ++upto2;
+  if (upto2 === 1000) {
+    count2.innerHTML = 10 + " M";
+    clearInterval(counts2);
+    
+  }
+}
+let counts3 = setInterval(updated3);
+let upto3 = 0;
+
+function updated3() {
+  var count3 = document.getElementById("counter3");
+
+  count3.innerHTML = ++upto3 + "/5";
+  if (upto3 === 5) {
+    clearInterval(counts3);
+  }
+}
+let counts4 = setInterval(updated4);
+let upto4 = 0;
+
+function updated4() {
+  var count4 = document.getElementById("counter4");
+
+  count4.innerHTML = ++upto4;
+  if (upto4 === 120) {
+    clearInterval(counts4);
+  }
+}
