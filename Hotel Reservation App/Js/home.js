c804f46e639a7b00ce50c7ebc4c576aab1b5ea95
islem mellehi <islemmellehi222@gmail.com>
2023-02-09 17:45:29 +0100

    Home page

--- Hotel Reservation App/Js/home.js
+++ Hotel Reservation App/Js/home.js
@@ -90,3 +90,27 @@ function updated4() {
     clearInterval(counts4);
   }
 }
+
+
+
+
+//read more button 
+function ReadMore(){
+  var dots = document.getElementById("dots")
+  var moreText = document.getElementById("more")
+  var btnText = document.getElementById("readBtn")
+  
+
+  if(dots.style.display === 'none'){
+    dots.style.display ="inline"
+    btnText.innerHTML ="Read More " 
+    moreText.style.display ="none"
+  }
+  else{
+    dots.style.display = "none"
+    btnText.innerHTML= "Read Less"
+    moreText.style.display="inline"
+  }
+}
+
+
